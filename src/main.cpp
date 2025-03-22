0f4d99dbfc95c0568674b68f8e060d0c174e15a8
Rafael A. <157764758+HarenDev@users.noreply.github.com>
2025-03-22 14:16:49 -0400

    Changed some of the methods to be more on par with Raylib-cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,14 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 int main ()
 {
-	// Tell the window to use vsync and work on high DPI displays
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
-	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	// Create the window and OpenGL context & tell the window to use vsync and work on high DPI displays
+	raylib::Window window(1024, 724, "Hello Raylib", FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	raylib::Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
@@ -35,10 +32,10 @@ int main ()
 		ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		raylib::DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		wabbit.Draw(400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
