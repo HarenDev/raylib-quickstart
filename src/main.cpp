9079ea3be838a5ff15a01995e387a54ba487b534
HarenDev <157764758+HarenDev@users.noreply.github.com>
2025-03-25 23:06:28 -0400

    Ditto

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,12 @@ int main ()
 	// Create the window and OpenGL context & tell the window to use vsync and work on high DPI displays (can be removed if necessary)
 	raylib::Window window(1280, 800, "Hello Raylib", FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	//We create an object program
-	Program program;
-
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	// When using Load() functions in subdirectorys, it should be used as Load("images/xyz.png") since resources is now the current 
 	SearchAndSetResourceDir("resources");
+
+	//We create an object program
+	Program program;
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
