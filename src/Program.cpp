c66c36af40216c2654c879d3607f19fb700a8d49
HarenDev <157764758+HarenDev@users.noreply.github.com>
2025-03-27 19:47:28 -0400

    Ditto

--- src/Program.cpp
+++ src/Program.cpp
@@ -30,9 +30,11 @@ void Program::CheckInput(){
 void Program::Unload(){
     //modify or replace this code with whatever you want :)
 
-    //Small Tip: Raylib seems to tend to unload certain things when the program is being closed (program variable goes out of scope due to
+    //Small Tip: Raylib tends to already know to unload assets when the program is being closed (program variable goes out of scope due to
     // the window closing), so force unloading may or may not lead to exceptions since it'll already have 
     // unloaded what your trying to unload, therefore it might be more ideal to unload specifically when you need to remove certain assets
     // while the program is running
+    // this is not to say Unload when your done running your program isn't necessary, its just a tip in case you run into some exceptions
+    // trying to use it
     wabbit.Unload();
 }
